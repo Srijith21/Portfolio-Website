f7f61f498167a45809a7745f8ba764f5c8e7b3c2
Srijith21 <srijithsreenivasan21@gmail.com>
2022-11-18 01:25:38 +0530

    new styles and features enabled

--- script.js
+++ script.js
@@ -20,3 +20,26 @@ function openmenu(){
 function closemenu(){
     side.style.right= "-200px";
 }
+
+
+var header = document.getElementById("header");
+var about = document.getElementById("about");
+var skills = document.getElementById("skills");
+var projects = document.getElementById("projects");
+var contact = document.getElementById("contact");
+
+
+function brightmode(){
+    header.style.backgroundColor='#fff';
+    about.style.backgroundColor='#fff';
+    skills.style.backgroundColor='#fff';
+    projects.style.backgroundColor='#fff';
+    contact.style.backgroundColor='#fff';
+}
+function darkmode(){
+    header.style.backgroundColor='#181818';
+    about.style.backgroundColor='#181818';
+    skills.style.backgroundColor='#181818';
+    projects.style.backgroundColor='#181818';
+    contact.style.backgroundColor='#181818';
+}
